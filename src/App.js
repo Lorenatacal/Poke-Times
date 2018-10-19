df951312f0414645d132bc58cfa4be116b099314
Lorenatacal <Lorenatacal@gmail.com>
2018-10-19 13:00:22 +0100

    Create the routes to the About and Contact coponents

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
 
 
 class App extends Component {
@@ -10,8 +12,9 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
             <Navbar />
-            <Route path='/' component={Home} />
-
+            <Route path='/home' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
         </div>
       </BrowserRouter>
     );
