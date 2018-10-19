8d104b51c55815cfb2954e7936b7a69c2bb11f49
Lorenatacal <Lorenatacal@gmail.com>
2018-10-19 13:18:05 +0100

    Change the Navbar Component by using Link and NavLink so we don't have to make additional request to the server

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="container">
-            <h4 clasName="center">Contact</h4>
+            <h4 className="center">Contact</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, expedita quaerat in ea delectus deserunt excepturi hic id quisquam quod facilis porro necessitatibus culpa, earum sunt enim, recusandae nulla doloremque?</p>
         </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="container">
-            <h4 clasName="center">Home</h4>
+            <h4 className="center">Home</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, expedita quaerat in ea delectus deserunt excepturi hic id quisquam quod facilis porro necessitatibus culpa, earum sunt enim, recusandae nulla doloremque?</p>
         </div>
     )
