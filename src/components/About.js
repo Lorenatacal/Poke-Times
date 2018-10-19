97e3cd4b462ca58d5651d750121da2743e87f7ef
Lorenatacal <Lorenatacal@gmail.com>
2018-10-19 12:37:00 +0100

    Create Home, About and Contact components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 clasName="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, expedita quaerat in ea delectus deserunt excepturi hic id quisquam quod facilis porro necessitatibus culpa, earum sunt enim, recusandae nulla doloremque?</p>
+        </div>
+    )
+}
+
+export default About
