245c937ac801b108bdb9bead9b3f4cb50c5e1479
Lorenatacal <Lorenatacal@gmail.com>
2018-11-01 14:25:41 +0000

    Connect post detail component to the redux store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,7 @@ const initState = {
     posts: [
         {id: '1', title: 'First title', body: 'This is my body'},
         {id: '2', title: 'Second title', body: 'This is the second body'},
-        {id: '3', title: 'First title', body: 'This is my body'},
+        {id: '3', title: 'Third title', body: 'This is my third body'},
     ]
 }
 
