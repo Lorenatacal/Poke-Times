f39560fe3418aa7bc29999b5785d4cb20ca74814
Lorenatacal <Lorenatacal@gmail.com>
2018-11-28 19:40:00 +0000

    Write test for addPost action

--- src/actions/postActions.test.js
+++ src/actions/postActions.test.js
@@ -17,3 +17,16 @@ test("deletePost should create an action to delete a post", () => {
     }
     expect(deletePost(id)).toEqual(expectedAction)
 })
+
+test("addPost should create an action to add a post", () => {
+    const id = "1"
+    const title = "My title"
+    const body = "My body"
+    const expectedAction = {
+        type: ADD_POST,
+        id,
+        title,
+        body,
+    }
+    expect(addPost(id, title, body)).toEqual(expectedAction)
+})
