412999d9c6eeb103ceb7c2891523783e031b0483
Lorenatacal <Lorenatacal@gmail.com>
2018-12-06 21:50:10 +0000

    Test the HomeConnected component

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -3,7 +3,9 @@ import HomeConnected, { Home } from './Home';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
-// import configureMockStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 Enzyme.configure({ adapter: new Adapter() })
 
@@ -26,5 +28,23 @@ test("Home should render correctly without posts", () => {
         comments: [],
     };
     const wrapper = Enzyme.shallow(<Home {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test("HomeConnected should render correctly", () => {
+    const initialState = {
+        posts: [
+            {id: '1', title: 'First title', body: 'This is my body'},
+            {id: '2', title: 'Second title', body: 'This is the second body'},
+            {id: '3', title: 'Third title', body: 'This is my third body'},
+        ],
+    };
+    const store = configureStore()(initialState);
+    const wrapper = Enzyme.mount(
+    <Provider store={store}>
+        <Router>
+            <HomeConnected />
+        </Router>
+    </Provider>);
     expect(toJson(wrapper)).toMatchSnapshot();
 });
