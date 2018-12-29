bd8526706779791e347e5b3c1d8f79e102f62526
Lorenatacal <Lorenatacal@gmail.com>
2018-12-29 22:25:35 +0000

    Create snapshot for App component

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import App from './App';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+test("App should render correctly", () => {
+    const wrapper = Enzyme.shallow(<App />)
+    expect(toJson(wrapper)).toMatchSnapshot();
+})
