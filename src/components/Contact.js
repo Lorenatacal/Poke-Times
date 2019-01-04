25e0316468bc3995485904841ec8211d75fa3755
Lorenatacal <Lorenatacal@gmail.com>
2019-01-04 16:29:36 +0000

    Style Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,57 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import ReactContactForm from 'react-mail-form';
+
+const StyledContainer = styled.div`
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto;
+    position: relative;
+`
+const StyledForm = styled.form`
+    background: #F9F9F9;
+    padding: 25px;
+    margin: 50px 0;
+    box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+`
+const StyledTitle = styled.h4`
+    color: #C62828;
+    margin-left: 40%;
+`
+const StyleButton = styled.button`
+    color: black;
+    font-size: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #C62828;
+    border-radius: 3px;
+    margin-left: 45%;
+`
+const StyledTextarea = styled.textarea`
+    font-size: 1.5em;
+`
+const StyledInput = styled.input`
+    font-size: 1.5em;
+`
 
 const Contact = (props) => {
     return (
-        <div className="container">
-            <h4 className="center">Contact</h4>
-            <div className="center post card">
-                <form>
-                    Surname: 
-                    <input  type="text" name="surname" placeholder="Type your surname here"/>
-                    First Name:
-                    <input type="Text" name="firstName" placeholder="Type your first name here"/>
-                    Email address:
-                    <input type="Text" name="email" placeholder="Type your email here"/>
-                    Message:
-                    <textarea placeholder="Please enter your message here" />
-                    <button className="center" type="Submit">Submit</button>
-                </form>
-            </div>
-        </div>
+        <StyledContainer>   
+            <styledDiv>
+                <StyledForm>
+                    <StyledTitle>Contact Us</StyledTitle>
+                    <label>
+                        <StyledInput type="Text" placeholder="Type your name here"/>
+                    </label>
+                    <label>
+                        <StyledInput type="Text" placeholder="Type your email here"/>
+                    </label>
+                    <label>
+                        <StyledTextarea placeholder="Please enter your message here" />
+                    </label>
+                    <StyleButton type="Submit">Submit</StyleButton>
+                </StyledForm>
+            </styledDiv>
+        </StyledContainer>
     )
 }
 
