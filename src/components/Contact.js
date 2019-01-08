c959988f266f910ea2a5fad6c50880745aef714c
Lorenatacal <Lorenatacal@gmail.com>
2019-01-08 14:00:19 +0000

    Create handleChange and handleSubmit events for the Contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,26 +39,52 @@ class Contact extends React.Component {
             name: '',
             email: '',
             message: '',
-        }  
+        }
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangeMessage = this.handleChangeMessage.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+    handleChangeName(event) {
+        this.setState({
+            name: event.target.value,
+        })
+    }
+
+    handleChangeEmail(event) {
+        this.setState({
+            email: event.target.value,
+        })
+    }
+
+    handleChangeMessage(event) {
+        this.setState({
+            message: event.target.value,
+        })
+    }
+
+    handleSubmit(event) {
+        alert("This" + this.state.name + this.state.email + this.state.message);
+    }
+
     render() {
         return (
             <StyledContainer>   
-                <styledDiv>
-                    <StyledForm>
+                <div>
+                    <StyledForm onSubmit={this.handleSubmit}>
                         <StyledTitle>Contact Us</StyledTitle>
                         <label>
-                            <StyledInput type="Text" placeholder="Type your name here"/>
+                            <StyledInput type="Text" value={this.state.value} onChange={this.handleChangeName} placeholder="Type your name here"/>
                         </label>
                         <label>
-                            <StyledInput type="Text" placeholder="Type your email here"/>
+                            <StyledInput type="Text" value={this.state.value} onChange={this.handleChangeEmail} placeholder="Type your email here"/>
                         </label>
                         <label>
-                            <StyledTextarea placeholder="Please enter your message here" />
+                            <StyledTextarea value={this.state.value} onChange={this.handleChangeMessage} placeholder="Please enter your message here" />
                         </label>
                         <StyleButton type="Submit">Submit</StyleButton>
                     </StyledForm>
-                </styledDiv>
+                </div>
             </StyledContainer>
         )
     }
